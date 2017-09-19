9439d9ceeac8b3743f131ebaf8ae4d2eba98b40a
horizon67 <sir.yes.sir.maru@gmail.com>
2017-09-19 23:03:48 +0900

    プラポリ実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index', defaults: { format: 'html' }
+  get "policy" => "policy#index"
   get "posts/:published_month" => 'posts#index', constraints: { :published_month => /[0-9]{4}-[0-9]{2}/ },  as: "posts_published_month"
   resources :posts, only: [:index, :show]
 end
---
+++ test/decorators/policy_decorator_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PolicyDecoratorTest < Draper::TestCase
+end
